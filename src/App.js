29c4dad66f2dee1a89cec9540b55a78c4f926d1a
xingl213 <hylxjackson@gmail.com>
2022-06-02 15:19:18 -0700

    changed hashrouter to browserrouter for now

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import Main from './components/Main';
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Main />
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 

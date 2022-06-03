575374063a5fe100232be18d886e0d011bb45975
xingl213 <hylxjackson@gmail.com>
2022-06-03 11:54:11 -0700

    changed Main into a functional component

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import Step1 from './Step1';
@@ -6,34 +6,23 @@ import Step2 from './Step2';
 import Step3 from './Step3';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
-class Main extends Component {
-	constructor(props) {
-		super(props);
+function Main() {
 
-		this.state = {
-		};
+	const callbackFunction = (childPiTagInfo) => {
 	}
 
-	callbackFunction = (childPiTagInfo) => {
-		this.setState({
-			piTagInfo: Object.assign({}, childPiTagInfo)
-		});
-	}
-
-	render() {
-		return (
-			<div>
-				<Header />
-				<Switch>
-					<Route path='/step1' component={() => <Step1 parentCallback={this.callbackFunction} />} />
-					<Route exact path='/step2' component={Step2} />
-					<Route exact path='/step3' component={Step3} />
-					<Redirect to="/step1" />
-				</Switch>
-				<Footer />
-			</div>
-		);
-	}
+	return (
+		<div>
+			<Header />
+			<Switch>
+				<Route path='/step1' component={() => <Step1 parentCallback={callbackFunction} />} />
+				<Route exact path='/step2' component={Step2} />
+				<Route exact path='/step3' component={Step3} />
+				<Redirect to="/step1" />
+			</Switch>
+			<Footer />
+		</div>
+	);
 }
 
 export default Main;
